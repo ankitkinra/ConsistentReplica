a7a4ab7276d8aae6aa4fe162f18e92301fd1fd81
Ashutosh Dhiman <ashutosh.dhiman@gmail.com>
2013-03-18 23:46:43 -0500

    Added Article and BulletinBoard parse

--- ConsistentReplicas/src/org/umn/distributed/consistent/common/Article.java
+++ ConsistentReplicas/src/org/umn/distributed/consistent/common/Article.java
@@ -1,15 +1,23 @@
 package org.umn.distributed.consistent.common;
 
+import org.apache.log4j.Logger;
+
 public class Article {
+	protected Logger logger = Logger.getLogger(this.getClass());
+
+	public static final int SHORT_TITLE_CHARS = 16;
+	public static final int SHORT_CONTENT_CHARS = 16;
+	public static final String FORMAT_START = "[";
+	public static final String FORMAT_END = "]";
 	private int id;
 	private int parentId;
 	private String title;
 	private String content;
-	
+
 	public Article() {
-		
+
 	}
-	
+
 	public Article(int id, int parentId, String title, String content) {
 		this.id = id;
 		this.parentId = parentId;
@@ -20,7 +28,7 @@ public class Article {
 	public int getId() {
 		return id;
 	}
-	
+
 	public int getParentId() {
 		return parentId;
 	}
@@ -32,13 +40,47 @@ public class Article {
 	public String getContent() {
 		return content;
 	}
-	
-	public static Article parseArticle(String articleStr) {
-		return new Article();
-	}
-	
+
 	@Override
 	public String toString() {
-		return null;
+		StringBuilder builder = new StringBuilder();
+		builder.append(Article.FORMAT_START).append("").append(id).append("|")
+				.append(parentId).append("|").append(title).append("|")
+				.append(content).append(Article.FORMAT_END);
+		return builder.toString();
+	}
+
+	public String toShortString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(Article.FORMAT_START).append(id).append("|")
+				.append(parentId).append("|")
+				.append(title.substring(0, Article.SHORT_TITLE_CHARS))
+				.append("|")
+				.append(content.substring(0, Article.SHORT_CONTENT_CHARS))
+				.append(Article.FORMAT_END);
+		return builder.toString();
+	}
+
+	public static Article parseArticle(String articleStr)
+			throws IllegalArgumentException {
+		if (!articleStr.startsWith(Article.FORMAT_START)
+				|| !articleStr.endsWith(Article.FORMAT_END)) {
+			throw new IllegalArgumentException("Invalid article format");
+		}
+		articleStr = articleStr.substring(1, articleStr.length() - 1);
+		String articleParams[] = articleStr.split("|");
+		if (articleParams.length != 4) {
+			throw new IllegalArgumentException(
+					"Invalid article parameter number");
+		}
+		int id = 0;
+		int parentId = 0;
+		try {
+			id = Integer.parseInt(articleParams[0]);
+			parentId = Integer.parseInt(articleParams[1]);
+		} catch (NumberFormatException nfe) {
+			throw new IllegalArgumentException("Invalid article id/parentId");
+		}
+		return new Article(id, parentId, articleParams[2], articleParams[3]);
 	}
 }
--- ConsistentReplicas/src/org/umn/distributed/consistent/common/BulletinBoard.java
+++ ConsistentReplicas/src/org/umn/distributed/consistent/common/BulletinBoard.java
@@ -1,100 +1,168 @@
 package org.umn.distributed.consistent.common;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.TreeMap;
 
 public class BulletinBoard {
 
-	private class BulletinBoardEntry {
-		private Article article;
-		private List<Integer> replyList;
-		
-		BulletinBoardEntry(Article article) {
-			this.article = article;
-		}
-		
-		Article getArticle( ) {
-			return article;
-		}
-		
-		List<Integer> getReplyIdList() {
-			return replyList;
-		}
-		
-		void addReplyId(int id) {
-			if(replyList == null) {
-				replyList = new ArrayList<Integer>();
-			}
-			replyList.add(id);
-		}
-	}
+	public static final String FORMAT_START = "{";
+	public static final String FORMAT_ENDS = "}";
 	private TreeMap<Integer, BulletinBoardEntry> map = new TreeMap<Integer, BulletinBoard.BulletinBoardEntry>();
-	
+
 	public boolean addArticle(Article article) {
-		//TODO: handle malicious coordinator. It should never return the same id back
+		// TODO: handle malicious coordinator. It should never return the same
+		// id back
 		map.put(article.getId(), new BulletinBoardEntry(article));
 		return true;
 	}
-	
+
 	public boolean addArticleReply(Article article) {
-		if(addArticle(article)) {
+		if (addArticle(article)) {
 			BulletinBoardEntry boardEntry = map.get(article.getParentId());
-			if(boardEntry == null) {
+			if (boardEntry == null) {
 				boardEntry = new BulletinBoardEntry(null);
 				boardEntry.addReplyId(article.getId());
 				map.put(article.getParentId(), boardEntry);
-			}
-			else {
+			} else {
 				boardEntry.addReplyId(article.getId());
 			}
-			//TODO: handle malicious coordinator. It should never return the same id back.
+			// TODO: handle malicious coordinator. It should never return the
+			// same id back.
 			return true;
 		}
 		return false;
 	}
-	
+
 	/*
-	 * Use this method to check if the BulletinBoard contains the id in the map or not. 
+	 * Use this method to check if the BulletinBoard contains the id in the map
+	 * or not.
 	 */
 	public boolean isArticle(int id) {
 		return map.containsKey(id);
 	}
-	
+
 	/*
-	 * Returns the article with a given id. This will return null if:
-	 * 1. there is not entry in the map for a given id
-	 * 2. article object is null corresponding to given id
-	 * Note that the second case will happen in case of quorum consistency. Even
-	 * though this method returns null, isArticle() can still return true (case 2).
+	 * Returns the article with a given id. This will return null if: 1. there
+	 * is not entry in the map for a given id 2. article object is null
+	 * corresponding to given id Note that the second case will happen in case
+	 * of quorum consistency. Even though this method returns null, isArticle()
+	 * can still return true (case 2).
 	 */
 	public Article getArticle(int id) {
 		BulletinBoardEntry boardEntry = map.get(id);
-		if(boardEntry == null) {
+		if (boardEntry == null) {
 			return null;
-		}
-		else {
+		} else {
 			return boardEntry.getArticle();
 		}
 	}
-	
+
 	public List<Integer> getReplyIdList(int id) {
 		BulletinBoardEntry boardEntry = map.get(id);
-		if(boardEntry == null) {
+		if (boardEntry == null) {
 			return null;
-		}
-		else {
+		} else {
 			return boardEntry.getReplyIdList();
 		}
 	}
-	
+
 	@Override
 	public String toString() {
-		return null;
+		return toString(true);
 	}
-	
-	
-	public static BulletinBoard parseBulletinBoard(String bulletinBoardStr) {
-		return new BulletinBoard();
+
+	public String toShortString() {
+		return toString(false);
+	}
+
+	/*
+	 * This method assumes that map is in synch
+	 */
+	private String toString(boolean detailed) {
+		StringBuilder builder = new StringBuilder();
+		// TODO: Check if returns an increasing order iterator or not.
+		Iterator<Integer> it = map.descendingKeySet().descendingIterator();
+		while (it.hasNext()) {
+			BulletinBoardEntry boardEntry = map.get(it.next());
+			Article article = boardEntry.getArticle();
+			if (article.getParentId() == 0) {
+				// TODO: create a copy of the object before passing here
+				appendToString(this, article.getId(), builder, detailed);
+			}
+		}
+		return builder.toString();
+	}
+
+	private static void appendToString(BulletinBoard board, Integer id,
+			StringBuilder builder, boolean detailed) {
+		builder.append("{");
+		BulletinBoardEntry entry = board.map.get(id);
+		if (detailed) {
+			builder.append(entry.getArticle().toString());
+		} else {
+			builder.append(entry.getArticle().toShortString());
+		}
+		for (Integer replyId : entry.getReplyIdList()) {
+			appendToString(board, replyId, builder, detailed);
+		}
+		builder.append("}");
+	}
+
+	public static BulletinBoard parseBulletinBoard(String boardStr) {
+		BulletinBoard board = new BulletinBoard();
+		parseBulletinbBoardEntries(boardStr, board);
+		return board;
+	}
+
+	private static String parseBulletinbBoardEntries(String boardStr,
+			BulletinBoard board) {
+		if (boardStr.length() <= 2) {
+			return null;
+		}
+		int index = -1;
+		Article article = null;
+		do {
+			index = boardStr.indexOf(Article.FORMAT_END);
+			article = Article.parseArticle(boardStr.substring(1, index + 1));
+			if (article.getParentId() == 0) {
+				board.addArticle(article);
+			} else {
+				board.addArticleReply(article);
+			}
+			boardStr = boardStr.substring(index + 1);
+			if (boardStr.startsWith(BulletinBoard.FORMAT_START)) {
+				boardStr = parseBulletinbBoardEntries(boardStr, board);
+			}
+			if (boardStr.startsWith(BulletinBoard.FORMAT_ENDS)) {
+				break;
+			}
+		} while (index != -1);
+		return boardStr.substring(1);
+	}
+
+	private class BulletinBoardEntry {
+		private Article article;
+		private List<Integer> replyList;
+
+		BulletinBoardEntry(Article article) {
+			this.article = article;
+		}
+
+		Article getArticle() {
+			return article;
+		}
+
+		List<Integer> getReplyIdList() {
+			return replyList;
+		}
+
+		void addReplyId(int id) {
+			if (replyList == null) {
+				replyList = new ArrayList<Integer>();
+			}
+			replyList.add(id);
+		}
 	}
 }

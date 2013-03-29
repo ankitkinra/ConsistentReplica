1ac25e6fc59a98d7c28fb71e7aa5fac89ab64961
Ashutosh Dhiman <ashutosh.dhiman@gmail.com>
2013-03-29 15:50:41 -0500

    run test file fix

--- ConsistentReplicas/src/runtests.sh
+++ ConsistentReplicas/src/runtests.sh
@@ -1,6 +1,6 @@
 if [ $JAVA_HOME ]
 then
-echo "Running PublisherServer"
+echo "Running tests...."
 java -classpath ../lib/log4j-1.2.17.jar:../lib/xmlpull-1.1.3.1.jar:../lib/xpp3_min-1.1.4c.jar:../lib/xstream-1.4.4.jar:. org/umn/distributed/consistent/common/client/testfrmwk/TestClient $1 $2 $3 $4
 else
 echo "JAVA_HOME not found"
